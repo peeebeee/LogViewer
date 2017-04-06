a3b65b66b5f9ea1388019331fe3532eedacb97d2
Peter Bowyer <g4mjs@hotmail.com>
2017-04-06 13:59:31 +0000

    Second commit

--- LogViewer/Form1.Designer.cs
+++ LogViewer/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Media.SolidColorBrush solidColorBrush1 = new System.Windows.Media.SolidColorBrush();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Hoverable = true;
+            this.cartesianChart1.Location = new System.Drawing.Point(155, 27);
+            this.cartesianChart1.Name = "cartesianChart1";
+            solidColorBrush1.Color = System.Windows.Media.Color.FromArgb(((byte)(30)), ((byte)(30)), ((byte)(30)), ((byte)(30)));
+            this.cartesianChart1.ScrollBarFill = solidColorBrush1;
+            this.cartesianChart1.ScrollHorizontalFrom = 0D;
+            this.cartesianChart1.ScrollHorizontalTo = 0D;
+            this.cartesianChart1.ScrollMode = LiveCharts.ScrollMode.None;
+            this.cartesianChart1.ScrollVerticalFrom = 0D;
+            this.cartesianChart1.ScrollVerticalTo = 0D;
+            this.cartesianChart1.Size = new System.Drawing.Size(1144, 516);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            this.cartesianChart1.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1327, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 44);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(137, 499);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 565);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- LogViewer/Form1.cs
+++ LogViewer/Form1.cs
@@ -7,14 +7,173 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.WinForms;
+using System.IO;
+using LiveCharts.Defaults;
+using System.Windows.Media;
 
 namespace LogViewer
 {
     public partial class Form1 : Form
     {
+        List<string> headerlist = new List<string>();
+        List<string> fieldlist = new List<string>();
+        List<double> timelist = new List<double>();
+        List<List<double>> datalist = new List<List<double>>();
+        List<Series> serieslist = new List<Series>();
+        System.Windows.Media.Brush[] Brushes =
+
+        {
+            System.Windows.Media.Brushes.Red,
+            System.Windows.Media.Brushes.Blue,
+            System.Windows.Media.Brushes.Green
+         };
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    string line;
+                    bool donefieldnames = false;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("#"))
+                        {
+                            headerlist.Add(line);
+                        }
+                        else
+                        {
+                            if (!donefieldnames)
+                            {
+                                parsefieldnames(line);                              
+                                donefieldnames = true;
+                                int i;
+                                for (i=0; i<fieldlist.Count; i++)
+                                {
+                                    datalist.Add ( new List<double>());
+                                }
+                            }
+                            else
+                            {
+                                ParseDataLine(line);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void parsefieldnames(string line)
+        {
+            string[] fieldnames = line.Split(',');
+            
+            fieldlist = fieldnames.Skip(1).ToList();
+            checkedListBox1.Items.AddRange(fieldlist.ToArray());
+        }
+        private void ParseDataLine(string line)
+        {
+            string[] datavalues = line.Split(',');
+
+            timelist.Add(Convert.ToDouble(datavalues[0]));
+            int i = 0;
+            foreach (string datavalue in datavalues.Skip(1))
+            {
+                datalist[i].Add(Convert.ToDouble(datavalue ));
+                serieslist.Add(CreateSeries(i));
+                i++;
+            }
+
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            AddToGraph(e);
+        }
+
+        private void AddToGraph(ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+            {
+                // MessageBox.Show(e.Index.ToString() + " Checked");
+                
+            }
+            else
+            {
+                // MessageBox.Show(e.Index.ToString() + " Unchecked");
+            }
+        }
+
+        private void UpdateChart()
+        {
+            cartesianChart1.Visible = false;
+            int seriescount = 0;
+            cartesianChart1.AxisX.Clear();
+            cartesianChart1.AxisX.Add(new Axis { Title = "Time (s)" });
+            cartesianChart1.AxisY.Clear();
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Zoom = ZoomingOptions.X;
+            for (int i=0; i<fieldlist.Count; i++)
+            {
+                
+                bool st = checkedListBox1.GetItemChecked(i);
+                if (st)
+                {
+                    Series s = CreateSeries(i);
+                    AxisPosition ap = ((seriescount & 1) == 0) ? AxisPosition.RightTop : AxisPosition.LeftBottom;
+                    cartesianChart1.AxisY.Add(new Axis { Title = fieldlist[i], Position = ap, Foreground = Brushes[seriescount % Brushes.Length] });
+                    s.Stroke = Brushes[seriescount % Brushes.Length];
+                    s.ScalesYAt = seriescount;
+                    cartesianChart1.Series.Add(s);
+                    
+                    seriescount++;
+
+                }
+                cartesianChart1.Visible = true;
+                
+            }
+        }
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private Series CreateSeries(int i)
+        {
+            List<ObservablePoint> lop = new List<ObservablePoint>();
+            for (int j=0; j<datalist[i].Count; j++)
+            {
+                ObservablePoint op = new ObservablePoint(timelist[j], datalist[i][j]);
+                lop.Add (op);
+                
+            }
+            Series s = new LineSeries { Values = new ChartValues<ObservablePoint>(lop.ToArray()),
+                PointGeometry = null,
+                StrokeThickness = 1,
+                LineSmoothness = 0,
+                Fill = new System.Windows.Media.SolidColorBrush { Opacity = 0 },
+                Title = fieldlist[i]
+            };
+            return (s);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateChart();
         }
     }
+
 }
